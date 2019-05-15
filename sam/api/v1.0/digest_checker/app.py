cb11ef501e839f79c66605ae3599619bd75ae3c3
Watcharapon Sarnsanit <pgolf1312@gmail.com>
2019-05-15 14:26:58 +0700

    app.py

--- sam/api/v1.0/digest_checker/app.py
+++ sam/api/v1.0/digest_checker/app.py
@@ -1,21 +1,27 @@
 from datasketch import MinHash, MinHashLSH, LeanMinHash
 import json
 import os
+import uuid
 
 
 def lambda_handler(event, context):
-    print(event)
-    print("---------------")
-    print(context)
-    print("VVVVVVVVVVVVVVV")
     redis_url, port = os.environ["REDIS_URL"].split(":")
     lsh = MinHashLSH(
         storage_config={
             'type': 'redis',
             'redis': {'host': redis_url, 'port': port}
         })
-    digest = event["digest"]
-    md5 = event["md5"]
+    uid = uuid.uuid4().hex
+    try:
+        digest = event["digest"]
+        md5 = event["md5"]
+    except:
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "error args"
+            })
+        }
     m1 = LeanMinHash.deserialize(digest)
     result = lsh.query(m1)
     if len(result) > 0:
@@ -26,7 +32,7 @@ def lambda_handler(event, context):
             }),
         }
     else:
-        lsh.insert(m1)
+        lsh.insert(key=uid, minhash=m1)
         return{
             "statusCode": 200,
             "body": json.dumps({
