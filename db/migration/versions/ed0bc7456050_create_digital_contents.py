77cda5d7c46dd716606d3ead81c05bb65cd31990
Watcharapon Sarnsanit <pgolf1312@gmail.com>
2019-06-07 18:35:21 +0700

    add size_file

--- db/migration/versions/ed0bc7456050_create_digital_contents.py
+++ db/migration/versions/ed0bc7456050_create_digital_contents.py
@@ -1,13 +1,12 @@
 """create digital contents
 
 Revision ID: ed0bc7456050
-Revises: 
+Revises:
 Create Date: 2019-05-16 16:18:42.969682
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'ed0bc7456050'
@@ -17,15 +16,14 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table(
-        "digital_contents",
-        sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('api_key_id', sa.String(255), nullable=False),
-        sa.Column('book_id', sa.String(255), nullable=False),
-        sa.Column('digest', sa.Text(), nullable=False),
-        sa.Column('sha256', sa.String(255), nullable=False),
-        sa.Index("book_id_index", "book_id", unique=True)
-    )
+    op.create_table("digital_contents",
+                    sa.Column('id', sa.Integer, primary_key=True),
+                    sa.Column('api_key_id', sa.String(255), nullable=False),
+                    sa.Column('book_id', sa.String(255), nullable=False),
+                    sa.Column('digest', sa.Text(), nullable=False),
+                    sa.Column('sha256', sa.String(255), nullable=False),
+                    sa.Column('size_file', sa.String(255), nullable=False),
+                    sa.Index("book_id_index", "book_id", unique=True))
 
 
 def downgrade():
--- sam/api/v1.0/digest_checker/app.py
+++ sam/api/v1.0/digest_checker/app.py
@@ -1,11 +1,11 @@
-from datasketch import MinHash, MinHashLSH, LeanMinHash
-import numpy as np
 import json
 import os
 import uuid
-import mysql.connector
-import boto3
 
+import boto3
+import mysql.connector
+import numpy as np
+from datasketch import LeanMinHash, MinHash, MinHashLSH
 
 ssm = boto3.client('ssm')
 
@@ -27,24 +27,27 @@ def convert_str_to_minhash(digest):
     return m1
 
 
-def insert_mysql(api_key_id=None, book_id=None, digest=None, sha256=None):
+def insert_mysql(api_key_id=None,
+                 book_id=None,
+                 digest=None,
+                 sha256=None,
+                 size_file=None):
     """Insert digital content to mysql for keeping information about the book.
     Args:
         api_key_id(string)  - Required  : api key from api gateway
         book_id(string)     - Required  : unique id we generate by time
         digest(string)      - Required  : digest from client
-        sha256(string)      - Rquireed  : sha256 generate from client
+        sha256(string)      - Required  : sha256 generate from client
+        size_file(string)   - Required  : size of file from client
     """
-    mydb = mysql.connector.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        passwd=DB_PASSWORD,
-        database="sixecho"
-    )
+    mydb = mysql.connector.connect(host=DB_HOST,
+                                   user=DB_USER,
+                                   passwd=DB_PASSWORD,
+                                   database="sixecho")
     mycursor = mydb.cursor()
 
-    sql = "INSERT INTO digital_contents (api_key_id, book_id, digest, sha256) VALUES (%s, %s,%s,%s)"
-    val = (api_key_id, book_id, digest, sha256)
+    sql = "INSERT INTO digital_contents (api_key_id, book_id, digest, sha256, size_file) VALUES (%s, %s,%s,%s,%s)"
+    val = (api_key_id, book_id, digest, sha256, size_file)
     mycursor.execute(sql, val)
     mydb.commit()
     mycursor.close()
@@ -57,7 +60,10 @@ def lambda_handler(event, context):
     lsh = MinHashLSH(
         storage_config={
             'type': 'redis',
-            'redis': {'host': redis_url, 'port': port},
+            'redis': {
+                'host': redis_url,
+                'port': port
+            },
             'basename': b'digital_checker',
         })
     uid = uuid.uuid4().hex
@@ -66,26 +72,25 @@ def lambda_handler(event, context):
     try:
         digest_str = body["digest"]
         sha256 = body["sha256"]
+        size_file = body["size_file"]
     except:
         return {
             "statusCode": 200,
-            "body": json.dumps({
-                "message": "error args"
-            })
+            "body": json.dumps({"message": "error args"})
         }
     m1 = convert_str_to_minhash(digest_str)
     result = lsh.query(m1)
     if len(result) > 0:
-        return{
+        return {
             "statusCode": 200,
             "body": json.dumps({
                 "message": "Duplicate",
             }),
         }
     else:
-        insert_mysql(api_key_id, uid, digest_str, sha256)
+        insert_mysql(api_key_id, uid, digest_str, sha256, size_file)
         lsh.insert(key=uid, minhash=m1)
-        return{
+        return {
             "statusCode": 200,
             "body": json.dumps({
                 "message": "Ok",
