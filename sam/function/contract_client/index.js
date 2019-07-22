58824cc2f0e17169b8b1bdb9c0f8f672cb546e32
Prach Hormanee <>
2019-07-22 18:37:31 +0700

    remove unnecsesary code

--- sam/function/contract_client/index.js
+++ sam/function/contract_client/index.js
@@ -14,22 +14,3 @@ if (process.env.NODE_ENV === 'test') {
   exports.handler = (event, context) =>
     awsServerlessExpress.proxy(server, event, context)
 }
-
-/*
-const Web3 = require('web3')
-const web3 = new Web3(process.env.NETWORK_PROVIDER_URL)
-
-exports.handler = async (event) => {
-  // TODO implement
-
-  web3.eth.getAccounts().then(e => { 
-    console.log("len: " + e.length);
-   }) 
-
-  const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
-};
-*/
