147f036b17e738e2f291004382cbad0cf4bf8a56
thanapol <thanapol@six.network>
2019-07-22 14:27:58 +0700

    init contract client

--- sam/function/contract_client/index.js
+++ sam/function/contract_client/index.js
@@ -3,6 +3,11 @@ const web3 = new Web3(process.env.NETWORK_PROVIDER_URL)
 
 exports.handler = async (event) => {
   // TODO implement
+
+  web3.eth.getAccounts().then(e => { 
+    console.log("len: " + e.length);
+   }) 
+
   const response = {
       statusCode: 200,
       body: JSON.stringify('Hello from Lambda!'),
