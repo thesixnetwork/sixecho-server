3aee689e7a6df8e6e5cbc0f8d065da3da34cc97b
Prach Hormanee <>
2019-07-26 15:59:15 +0700

    remove web3 lib

--- sam/function/contract_client/src/router/wallet.route.js
+++ sam/function/contract_client/src/router/wallet.route.js
@@ -1,21 +1,6 @@
 const express = require('express')
-const Web3 = require('web3')
-const web3 = new Web3(process.env.NETWORK_PROVIDER_URL)
 const router = express.Router()
-const Handler = require('../middleware/handler.middleware')
 
-router.route('/accounts').get((req, res, next) => {
-  const handler = new Handler()
-  web3.eth
-    .getAccounts()
-    .then(e => {
-      handler.setResponseBody(e).setStatusCode(200)
-      next(handler)
-    })
-    .catch(e => {
-      handler.setErrorMessage(e.message).setStatusCode(400)
-      next(handler)
-    })
-})
+router.route('/accounts').get((req, res, next) => {})
 
 module.exports = router
