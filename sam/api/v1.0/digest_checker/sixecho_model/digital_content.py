9267023dd54771f16c2157bef4ff03c8d35de970
watcharapon <lolgolflol@Watcharapons-MacBook-Pro.local>
2019-08-22 17:42:00 +0700

    digital content

---
+++ sam/api/v1.0/digest_checker/sixecho_model/digital_content.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class DigitalContent(Base):
+    __tablename__ = 'digital_contents'
+    id = Column(String, primary_key=True)
+    api_key_id = Column(String)
+    category_id = Column(Integer)
+    publish_id = Column(Integer)
+    digital_content_id = Column(String)
+    title = Column(String)
+    digest = Column(String)
+    sha256 = Column(String)
+    size_file = Column(String)
+    content_type = Column(String)
+    meta_media = Column(String)
+    author = Column(String)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
