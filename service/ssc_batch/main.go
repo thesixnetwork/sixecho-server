a8e5d6efa308fd0b614258833da53de3e4563a51
watcharapon <pgolf1312@gmail.com>
2019-10-02 15:38:18 +0700

    elastic

--- service/ssc_batch/main.go
+++ service/ssc_batch/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
+	"github.com/elastic/go-elasticsearch"
 	"github.com/eoscanada/eos-go"
 )
 
 var (
 	api             *eos.API
+	es              *elasticsearch.Client
 	currentBlockNum uint32
 )
 
@@ -17,7 +20,7 @@ func tailBlock(block chan *eos.BlockResp, blockNum chan uint32) {
 		select {
 		case num := <-blockNum:
 			blockResp, _ := api.GetBlockByNum(num)
-			fmt.Println(blockResp.BlockNum)
+			// fmt.Println(blockResp.BlockNum)
 			block <- blockResp
 		}
 	}
@@ -28,7 +31,8 @@ func updateBlockNumToES() {
 }
 
 func invokeLambda(data *eos.BlockResp) {
-	fmt.Println("Invokde lambda")
+	fmt.Printf("%#v\n", data.ID.String())
+	// fmt.Println("Invokde lambda")
 }
 
 func loadAllBackgroundProcess(block chan *eos.BlockResp, blockNum chan uint32) {
@@ -62,7 +66,30 @@ func getCurrentBlockNum() {
 	currentBlockNum = infoResp.HeadBlockNum
 }
 
+func createIndexElastic() {
+	fmt.Println("Load elasticsearch...")
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"https://search-es-six-zunsizmfamv7eawswgdvwmyd6u.ap-southeast-1.es.amazonaws.com",
+		},
+	}
+	var err error
+	es, err = elasticsearch.NewClient(cfg)
+
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+	res, err := es.Info()
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	log.Println(res)
+	createSSCBlockNumIndex(es)
+	createSSCDigitalContentIndex(es)
+}
+
 func main() {
+	createIndexElastic()
 	block := make(chan *eos.BlockResp)
 	blockNum := make(chan uint32)
 	api = eos.New("http://ec2-3-0-89-218.ap-southeast-1.compute.amazonaws.com:8888")
