510a537b429b2245c37293ac780e85db3b41be5d
watcharapon <pgolf1312@gmail.com>
2020-01-02 12:46:29 +0700

    timeout

--- sam/function/generate_wallet/index.js
+++ sam/function/generate_wallet/index.js
@@ -1,6 +1,10 @@
 const Caver = require('caver-js');
 const AWS = require('aws-sdk');
-const caver = new Caver(process.env.NETWORK_PROVIDER_URL);
+const caver = new Caver(
+  new Caver.providers.HttpProvider(process.env.NETWORK_PROVIDER_URL, {
+    timeout: 44000
+  })
+);
 const EchoAPI = require('./APIv101.json');
 const _ = require('lodash');
 const echoAPI = new caver.klay.Contract(
@@ -21,8 +25,8 @@ function setFeePayerAccount() {
   const options3 = { Name: 'SK_ECHO_WALLET', WithDecryption: true };
   if (account && feePayer) {
     return new Promise((resolve, reject) => {
-      resolve([account, feePayer])
-    })
+      resolve([account, feePayer]);
+    });
   }
   const feeaccount = new Promise((resolve, reject) => {
     ssm.getParameter(options, (err, data) => {
@@ -57,7 +61,7 @@ function setFeePayerAccount() {
     });
   });
   return Promise.all([feeaccount, privateKey, defaultAccount]).then(values => {
-    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
     caver.klay.accounts.wallet.clear();
     caver.klay.accounts.wallet.add(values[1], values[0]);
     account = caver.klay.accounts.wallet.add(values[2]);
@@ -68,47 +72,59 @@ function setFeePayerAccount() {
 
 exports.handler = (event, context, callback) => {
   const number = _.get(event, 'number', 1);
-  setFeePayerAccount().then((data) => {
-    const defaultAccount = data[0]
-    const feepayer = data[1]
-    const promises = [];
+  setFeePayerAccount().then(
+    data => {
+      const defaultAccount = data[0];
+      const feepayer = data[1];
+      const promises = [];
 
-    caver.klay.getTransactionCount(defaultAccount.address).then(nonce => {
-      for (var i = 0; i < number; i++) {
-        const account = caver.klay.accounts.create();
-        const data = echoAPI.methods.addWriter(account.address).encodeABI();
-        const klayRequest = caver.klay.accounts.signTransaction({
-            type: 'FEE_DELEGATED_SMART_CONTRACT_EXECUTION',
-            from: defaultAccount.address,
-            to: process.env.API_CONTRACT_ADDRESS,
-            data,
-            gas: 10000000,
-            nonce: nonce + i
-          },
-          defaultAccount.privateKey
-        ).then(result => {
-          const { rawTransaction: senderRawTransaction } = result;
-          return caver.klay
-            .sendTransaction({
-              senderRawTransaction: senderRawTransaction,
-              feePayer: feepayer
-            })
+      caver.klay.getTransactionCount(defaultAccount.address).then(nonce => {
+        for (var i = 0; i < number; i++) {
+          const account = caver.klay.accounts.create();
+          const data = echoAPI.methods.addWriter(account.address).encodeABI();
+          const klayRequest = caver.klay.accounts
+            .signTransaction(
+              {
+                type: 'FEE_DELEGATED_SMART_CONTRACT_EXECUTION',
+                from: defaultAccount.address,
+                to: process.env.API_CONTRACT_ADDRESS,
+                data,
+                gas: 10000000,
+                nonce: nonce + i
+              },
+              defaultAccount.privateKey
+            )
             .then(
               result => {
-                return account;
+                const { rawTransaction: senderRawTransaction } = result;
+                return caver.klay
+                  .sendTransaction({
+                    senderRawTransaction: senderRawTransaction,
+                    feePayer: feepayer
+                  })
+                  .then(
+                    result => {
+                      return account;
+                    },
+                    err => {
+                      console.log(err);
+                      return err;
+                    }
+                  );
               },
               err => {
-                console.log(err)
                 return err;
               }
             );
-
-        }, err => { return err; });
-        promises.push(klayRequest);
-      }
-      Promise.all(promises).then(accounts => {
-        callback(null, accounts);
+          promises.push(klayRequest);
+        }
+        Promise.all(promises).then(accounts => {
+          callback(null, accounts);
+        });
       });
-    })
-  }, error => { callback(error) });
+    },
+    error => {
+      callback(error);
+    }
+  );
 };
